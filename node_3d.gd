fbd4b1d4032e92c60d11a84fc68631b5a2bd98bd
Fl9b0y <jmuda918@gmail.com>
2025-05-29 10:17:05 +0200

    audio player

--- node_3d.gd
+++ node_3d.gd
@@ -1,8 +1,18 @@
 extends RigidBody3D
+@onready var death: AudioStreamPlayer = $death
+@onready var success: AudioStreamPlayer = $success
+@onready var engine_thrust: AudioStreamPlayer3D = $engine_thrust
+
+
+
 var is_transitioning : bool = false
 func movement(delta) ->void:
 	if Input.is_action_pressed("ui_up"):
 		apply_central_force(basis.y*delta*1000)
+		if engine_thrust.playing == false:
+			engine_thrust.play()
+	else:
+		engine_thrust.stop()
 	if Input.is_action_pressed("ui_down"):
 		apply_central_force(-basis.y*delta*1000)
 	if (Input.is_action_pressed("ui_right") )and (Input.is_action_pressed("ui_up")) :
@@ -28,17 +38,19 @@ func _on_body_entered(body: Node) -> void:
 
 func win_sequecne(next_level:String)-> void:
 	print("you win ")
+	success.play()
 	is_transitioning = true
 	set_process(false)
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.0)
 	tween.tween_callback(get_tree().change_scene_to_file.bind(next_level))
 	
 	
 func crash_sequence()-> void:
 	print("you lose ")
+	death.play()
 	is_transitioning = true
 	set_process(false)
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.0)
 	tween.tween_callback(get_tree().reload_current_scene)
